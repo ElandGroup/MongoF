a6d41b390dae6ee666a30cfc7bae8e5e59f859eb
unknown <xiaoxm_001@163.com>
2016-05-14 11:05:32 +0800

    import Mongodb

---
+++ src/MongoF/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoF
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //连接信息
+            string conn = "mongodb://10.202.101.43:27017";
+            var client = new MongoClient(conn);
+            var database = client.GetDatabase("HelloMongo");
+            var collection = database.GetCollection<BsonDocument>("Order");
+
+            //1.Init query
+            Console.WriteLine("---1.query from mongdb");
+            OutputAll(collection);
+
+            //2. insert one and query all
+            Console.WriteLine("==press enter,insert one data  enter");
+            Console.ReadLine();
+            var document = new BsonDocument {
+                { "name", "xiaomiao" },
+                { "sex", "male" }
+            };
+            collection.InsertOne(document);
+            Console.WriteLine("---2.query after insert");
+            OutputAll(collection);
+
+            //3. insert many and query all
+            Console.WriteLine("==input count,like:2,will insert 2 row data");
+            string insertmanys = Console.ReadLine();
+            var documents = Enumerable.Range(0, 2).Select(i => new BsonDocument("counter", i));
+            collection.InsertMany(documents);
+            Console.WriteLine("---3.query after insert 2 row data");
+            OutputAll(collection);
+
+            //4. update and query all
+            Console.WriteLine("==press enter,update xiaomiao's info");
+            Console.ReadLine();
+            var filterUpdate = Builders<BsonDocument>.Filter.Eq("name", "xiaomiao");
+            var update = Builders<BsonDocument>.Update.Set("sex ", "x-male");
+            collection.UpdateOne(filterUpdate, update);
+            Console.WriteLine("---4.query after update data");
+            OutputAll(collection);
+
+            //5. delete one and query all
+            Console.WriteLine("==input counter you want to delete,like 1");
+            string deleteCounter = Console.ReadLine();
+            var filter = Builders<BsonDocument>.Filter.Eq("counter", Convert.ToInt32(deleteCounter));
+            collection.DeleteOne(filter);
+            Console.WriteLine("---5.query after delete data");
+            OutputAll(collection);
+
+
+
+            Console.ReadLine();
+
+
+        }
+
+        private static void OutputAll(IMongoCollection<BsonDocument> collection)
+        {
+            var cursor = collection.Find(new BsonDocument()).ToCursor();
+            foreach (var document in cursor.ToEnumerable())
+            {
+                Console.WriteLine(document);
+            }
+        }
+
+    }
+}
---
+++ src/MongoF/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MongoF")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MongoF")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2e3880eb-3ddf-4444-8cc6-732fa2113776")]
